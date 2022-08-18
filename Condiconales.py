9c08c855ed4603c39311c89379a1b61713cb6e96
EduFF34 <fragoso.ed@ciencias.unam.mx>
2022-08-18 11:58:44 -0500

    Tercer commit

--- Condiconales.py
+++ Condiconales.py
@@ -1,15 +1,1 @@
-print('Segundo commit')
-
-#def evaluacion(nota):
-#	valoracion='aprobado'
-#	if nota<=5:
-#		valoracion='reprobado'
-#	return valoracion
-
-#nota_alumno=input('Introduca la nota del alumno: ')
-
-#print(evaluacion(int(nota_alumno)))
-
-
-
-#input()
+print('Tercer commit')
